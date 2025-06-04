0f092ffbb1ce865e84311a2396af7501af9d713c
Aslan049 <aslankorkmaz049@gmail.com>
2025-06-04 10:16:56 +0300

    upload project

---
+++ GuessFlag/GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Aslan Korkmaz on 2.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
